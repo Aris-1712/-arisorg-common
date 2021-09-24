b3d0431fda56d68ee6d84c6dfa2019dbbf835174
Aris Shaikh <arisshaikh@Ariss-MacBook-Pro.local>
2021-09-24 17:28:09 +0400

    commit

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,7 @@
-export * from './Errors/GeneralErrors';
+import GeneralErrors from './Errors/GeneralErrors';
+import InvalidParamsError from './Errors/InvalidParamsError';
+import ErrorHandler from './Middleware/ErrorHandler';
+export { GeneralErrors, InvalidParamsError, ErrorHandler };
 export * from './Middleware/ErrorHandler';
 export * from './utils/auth';
 export * from './Errors/InvalidParamsError';
--- build/index.js
+++ build/index.js
@@ -9,8 +9,17 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./Errors/GeneralErrors"), exports);
+exports.ErrorHandler = exports.InvalidParamsError = exports.GeneralErrors = void 0;
+const GeneralErrors_1 = __importDefault(require("./Errors/GeneralErrors"));
+exports.GeneralErrors = GeneralErrors_1.default;
+const InvalidParamsError_1 = __importDefault(require("./Errors/InvalidParamsError"));
+exports.InvalidParamsError = InvalidParamsError_1.default;
+const ErrorHandler_1 = __importDefault(require("./Middleware/ErrorHandler"));
+exports.ErrorHandler = ErrorHandler_1.default;
 __exportStar(require("./Middleware/ErrorHandler"), exports);
 __exportStar(require("./utils/auth"), exports);
 __exportStar(require("./Errors/InvalidParamsError"), exports);
